c89695a360e0a6ebaf191ef588b9977c2e0c49ca
vsvsv <vsevolodplatunov@gmail.com>
2024-04-24 00:11:09 +0300

    Addtitonal window hints

--- src/main.zig
+++ src/main.zig
@@ -28,6 +28,9 @@ pub fn initWindow(width: u32, height: u32, title: []const u8) !*c.GLFWwindow {
     c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 4);
     c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 1);
 
+    c.glfwWindowHint(c.GLFW_OPENGL_PROFILE, c.GLFW_OPENGL_CORE_PROFILE);
+    c.glfwWindowHint(c.GLFW_OPENGL_FORWARD_COMPAT, c.GLFW_TRUE);
+
     c.glfwMakeContextCurrent(window);
     c.glfwSwapInterval(1);
 
@@ -43,7 +46,7 @@ pub fn main() !void {
 
     c.glClearColor(0.0, 0.0, 1.0, 1.0);
 
-    while (c.glfwWindowShouldClose(window) == 0) {
+    while (c.glfwWindowShouldClose(window) != c.GLFW_TRUE) {
         c.glClear(c.GL_COLOR_BUFFER_BIT);
         c.glfwSwapBuffers(window);
         c.glfwPollEvents();
